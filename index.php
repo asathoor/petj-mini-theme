e29befba0223ca78e7e674904e05b5e05082f35f
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2017-04-19 08:32:28 +0200

    Commented code
    with suggestions and links to more info.

--- index.php
+++ index.php
@@ -11,8 +11,11 @@
 if ( have_posts() ) {
     while ( have_posts() ) {
         the_post();
-        the_title( '<h3>', '</h3>' );
-        the_content(); // add markup before and after the content
+	    // add template tags see @url https://codex.wordpress.org/Template_Tags
+	    // a few samples:
+            the_title( '<h3>', '</h3>' );
+            the_content(); // you can wrap the "template tags" in markup, like <article> or <div>
+	    // on your own: try to add info like date, author, comments etc.
     }
 }
 ?>
